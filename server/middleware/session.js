070a6c40374254c69fe60cbc0f0aa1f6560cc745
Harish Mohan <harish@bytize.in>
2022-11-10 13:53:25 +0530

    redis cluster support for session

--- server/middleware/session.js
+++ server/middleware/session.js
@@ -1,23 +1,32 @@
-import { createClient } from 'redis';
+import { createClient, createCluster } from 'redis';
 import session from 'express-session';
 import connectRedis from 'connect-redis';
 
 const RedisStore = connectRedis(session);
-const redisClient = createClient({
-  legacyMode: true,
-  socket: {
-    host: process.env.REDIS_HOST,
-    port: process.env.REDIS_PORT,
-  },
-  database: process.env.REDIS_DB,
-});
+const redisConnection =
+  process.env.REDIS_CONNECTION_TYPE === 'cluster'
+    ? createCluster({
+        rootNodes: [
+          {
+            url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+          },
+        ],
+      })
+    : createClient({
+        legacyMode: true,
+        socket: {
+          host: process.env.REDIS_HOST,
+          port: process.env.REDIS_PORT,
+        },
+        database: process.env.REDIS_DB,
+      });
 
-redisClient.connect().catch(console.error);
+redisConnection.connect().catch(console.error);
 
 export default fromNodeMiddleware((req, res, next) => {
   if (!req.url.startsWith('/__nuxt_error')) {
     return session({
-      store: new RedisStore({ client: redisClient }),
+      store: new RedisStore({ client: redisConnection }),
       secret: process.env.SESSION_SECRET,
       proxy: process.env.NODE_ENV !== 'development',
       resave: false,
